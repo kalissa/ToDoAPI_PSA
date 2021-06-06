0ca54d393d8f0e3a583969c11c2fff54397d574a
Henrique Ramires <31447784+hramires@users.noreply.github.com>
2021-06-06 15:24:47 -0300

    Definindo routefix como vazia

--- ToDoAPI_PSA/Startup.cs
+++ ToDoAPI_PSA/Startup.cs
@@ -43,8 +43,13 @@ namespace ToDoAPI_PSA
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDoAPI_PSA v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                    c.RoutePrefix = string.Empty;
+
+                });
             }
 
             app.UseHttpsRedirection();
